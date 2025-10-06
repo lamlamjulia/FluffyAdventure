6b60123e9e874415ffcdcf7423abab3d1454100b
LamLam <ailamtran1812@gmail.com>
2025-10-06 16:46:39 +0200

    added Gameloop and keyhandling

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,6 +1,6 @@
 package main;
-import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements Runnable{
@@ -18,8 +18,17 @@ public class GamePanel extends JPanel implements Runnable{
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
     Thread gameThread;
+    int FPS = 60;
+    int playerX = 0;
+    int playerY = 0;
+    int playerSpeed = 3;
+
+    KeyHandler keyHandler = new KeyHandler();
+
     public void startGameThread()
     {
         gameThread = new Thread(this);
@@ -28,9 +37,55 @@ public class GamePanel extends JPanel implements Runnable{
 
     @Override
     public void run() {
+        double drawInterval = 1000000000/FPS;
+
+        long lastTime = System.nanoTime();
+        long currentTime;
+        double delta = 0;
+        int drawCount = 0;
+        long timer = 0;
+
         while(gameThread != null)
         {
-            //do sth
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime)/drawInterval;
+            timer = currentTime - lastTime;
+            lastTime = currentTime;
+            if(delta >= 1)
+            {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+            if(timer >= 1000000000)
+            {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
         }
+    }
+    public void update()
+    {
+        if(keyHandler.upPressed){
+            playerY -= playerSpeed;
+        }
+        else if(keyHandler.downPressed){
+            playerY += playerSpeed;
+        }
+        else if(keyHandler.leftPressed){
+            playerX -= playerSpeed;
+        }
+        else if(keyHandler.rightPressed){
+            playerX += playerSpeed;
+        }
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.white);
+        g2d.fillRect(playerX, playerY, tileSize, tileSize);
+        g2d.dispose();
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("Yona's Adventure");
+        window.setTitle("Fluffy's Adventure");
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
@@ -15,6 +15,8 @@ public class Main {
 
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        gamePanel.startGameThread();
 
     }
 }
